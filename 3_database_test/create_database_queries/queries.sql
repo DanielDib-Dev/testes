3dc73def8b8bff7033e3bdb92561f5bd1f38a53e
Daniel Dib <138417200+DanielDib-Dev@users.noreply.github.com>
2025-04-04 10:15:47 -0300

    feat: new create table query

--- 3_database_test/create_database_queries/queries.sql
+++ 3_database_test/create_database_queries/queries.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS ans_dados;
 USE ans_dados;
 
--- Criação da tabela
+-- Criação das tabelas
 CREATE TABLE demonstracoes_contabeis (
     data_ref DATE,
     reg_ans INT,
@@ -10,4 +10,27 @@ CREATE TABLE demonstracoes_contabeis (
     descricao VARCHAR(255),
     vl_saldo_inicial DECIMAL(20, 2),
     vl_saldo_final DECIMAL(20, 2)
+);
+
+CREATE TABLE IF NOT EXISTS operadoras (
+    registro_ans INT PRIMARY KEY,
+    cnpj VARCHAR(20),
+    razao_social VARCHAR(255),
+    nome_fantasia VARCHAR(255),
+    modalidade VARCHAR(100),
+    logradouro VARCHAR(255),
+    numero VARCHAR(20),
+    complemento VARCHAR(100),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    uf CHAR(2),
+    cep VARCHAR(10),
+    ddd VARCHAR(5),
+    telefone VARCHAR(15),
+    fax VARCHAR(15),
+    endereco_eletronico VARCHAR(255),
+    representante VARCHAR(255),
+    cargo_representante VARCHAR(100),
+    regiao_de_comercializacao TINYINT,
+    data_registro_ans DATE
 );
